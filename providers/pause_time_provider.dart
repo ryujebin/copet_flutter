86558300d6c241e388bbdb7b3dbd770a6ef54e65
ryujebin <129826948+ryujebin@users.noreply.github.com>
2025-01-29 17:28:43 +0900

    Delete providers directory

--- providers/pause_time_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final PauseTimeProvider = StateProvider<int>(
-        (ref) => 0
-);
--- providers/walk_time_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final WalkTimeProvider = StateProvider<int>(
-        (ref) => 0
-);
