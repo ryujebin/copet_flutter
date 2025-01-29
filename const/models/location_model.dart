d94a72be5f251e26fd7014fdb6cbd5dedc057429
ryujebin <129826948+ryujebin@users.noreply.github.com>
2025-01-29 17:27:49 +0900

    Delete const directory

--- const/models/location_model.dart
+++
@@ -1,9 +0,0 @@
-class LocationModel {
-  late double latitude;
-  late double longitude;
-
-  LocationModel({
-    required this.latitude,
-    required this.longitude
-});
-}
--- const/regions/region_list.dart
+++
@@ -1,9 +0,0 @@
-import 'package:pet/const/models/region_model.dart';
-import 'package:pet/const/regions/jellanamdo/jeollanamdo.dart';
-import 'package:pet/const/regions/jeollabukdo/jeollabukdo.dart';
-
-List<State_> regionList = [
-  jeollanamdo,
-  jeollabukdo
-];
-
